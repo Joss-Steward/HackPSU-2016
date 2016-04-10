79eba96e8d1ce7e898862921011c1e2de56af41b
Joss-Steward <joss.steward@gmail.com>
2016-04-10 00:52:33 -0400

    Rigged it to add all new users to a default group

--- HackPSU-2016/Controllers/GroupController.cs
+++ HackPSU-2016/Controllers/GroupController.cs
@@ -1,13 +1,34 @@
-﻿using System;
+﻿using HackPSU_2016.Models;
+using Microsoft.AspNet.Identity.Owin;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace HackPSU_2016.Controllers{
-    public class GroupController : Controller{
+namespace HackPSU_2016.Controllers
+{
+    public class GroupController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
 
-        public ActionResult Index(int? Id){
+        private ApplicationUserManager _userManager;
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+        // GET: Group
+        public ActionResult Index()
+        {
             return View();
         }
     }

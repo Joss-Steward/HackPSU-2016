572628d83bb7c0767f351313790301b3a4d1ae99
Evan Stevenson <evanstevenson13@gmail.com>
2016-04-10 02:40:25 -0400

    Styles

--- HackPSU-2016/Controllers/AccountController.cs
+++ HackPSU-2016/Controllers/AccountController.cs
@@ -228,7 +228,7 @@ namespace HackPSU_2016.Controllers
                         }
                         throw;
                     }
-                    return View("Login");
+                    return RedirectToAction("Login","Account");
                 }
                 AddErrors(result);
             }
